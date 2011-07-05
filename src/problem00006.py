402ebcd88aab2aa843597744e2907452427815f3
Karlo Smid <karlo.smid@gmail.com>
2011-07-05 20:19:47 +0200

    problem 6 with formulas

--- src/problem00006.py
+++ src/problem00006.py
@@ -5,17 +5,13 @@ __author__="karlo"
 __date__ ="$Jun 25, 2011 6:13:28 PM$"
 
 
-def sumOfSquares(n):
-    rez = 0
-    for i in n:
-        rez += i*i
-    return rez
+def sumOfSquares(n):    
+    return (n/6.)*(2*n+1)*(n+1)
 
-def squareOfSums(n):
-    rez = 0
-    for i in n:
-        rez += i
-    return rez*rez
+def squareOfSums(n):        
+    #euler
+    sum = n*(n+1)/2
+    return sum*sum
 if __name__ == "__main__":
-    n = range(101)
+    n = 1000
     print (squareOfSums(n) - sumOfSquares(n))
