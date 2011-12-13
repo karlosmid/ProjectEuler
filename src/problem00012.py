93fe5d2c910a53bc4c471caf519ebf13023484c2
Karlo Smid <karlo.smid@gmail.com>
2011-12-13 22:46:02 +0100

    finished improved primes. Regular primes is slower than basic improved algorithm. Investigate.

--- src/problem00012.py
+++ src/problem00012.py
@@ -9,9 +9,11 @@ import problem00010
 class TriangleNumber:
     #http://projecteuler.net/problem=12, need to register on home page.
 
-    def __init__(self):
+    def __init__(self,noOfPrimes):
         self.sieveImproved = None
         self.lOfCrossedPrimes = []
+        self.noOfPrimes = noOfPrimes
+        self.sieveImproved = problem00010.SieveAlgorithm(self.noOfPrimes)
 
     def calcTriangle(self,n):
         return n*(n+1)/2
@@ -23,63 +25,73 @@ class TriangleNumber:
                 number = number/prime
         return primeExponent
 
-    def calcNumberOfDivisorsUsingBruteForce(self,n):
-        triangleNumber = self.calcTriangle(n)
-        if triangleNumber > 1:
+    def calcNumberOfDivisorsUsingBruteForce(self,n):        
+        if n > 1:
             noOfDivisors = 2
         else:
             noOfDivisors = 1
-        for number in range(2,triangleNumber):
-            if triangleNumber % number == 0:
+        for number in range(2,n):
+            if n % number == 0:
                 noOfDivisors = noOfDivisors + 1
         return noOfDivisors
 
-    def calcNumberOfDivisorsUsingPrimes(self,n):
-        self.sieveImproved = problem00010.SieveAlgorithm(65500)        
-        self.lOfCrossedPrimes = self.sieveImproved.listOfCrossedPrimesWhereIndexIsPrimeValue
-        triangleNumber = self.calcTriangle(n)
-        noOfDivisors = 1        
-#        print self.lOfCrossedPrimes
+    def calcNumberOfDivisorsUsingPrimes(self,n):        
+        self.lOfCrossedPrimes = self.sieveImproved.listOfCrossedPrimesWhereIndexIsPrimeValue        
+        noOfDivisors = 1
         primeValue = 0
         for primeCrossedFlag in self.lOfCrossedPrimes:
             if self.sieveImproved.isCrosed(primeCrossedFlag):                
                 primeExponent = 0
-                primeExponent = self.calcPrimeExponent(primeValue,triangleNumber)
+                primeExponent = self.calcPrimeExponent(primeValue,n)
                 if primeExponent > 0:
                     noOfDivisors = (primeExponent + 1) * noOfDivisors
             primeValue = primeValue + 1
         return noOfDivisors
 
-
-    def calcNumberOfDivisors(self,n):        
-        if n == 1:
+    def calcNumberOfDivisorsForTriangleNumber(self,upToN):
+        triangleNumber = self.calcTriangle(upToN)
+        if upToN == 1:
             return 1
-        elif n == 2:
+        elif upToN == 2:
             return 2
-#        noOfDivisors = 2
-        noOfDivisors = 0
-        triangleNumber = self.calcTriangle(n)
-#        upperLimit = n/2 + 1
-        upperLimit = n
+        noOfDivisors = 0        
+        upperLimit = upToN
         for num in range(1,upperLimit + 1):
-             if triangleNumber%num == 0:            
-                 if triangleNumber/num > n:
+             if triangleNumber%num == 0:
+                 if triangleNumber/num > upToN:
                      noOfDivisors = noOfDivisors + 2
                  else:
                      noOfDivisors = noOfDivisors + 1
         return noOfDivisors    
-    def findFirstTriangleNumberWithGraterNumberOfDivisors(self,noOfDivisorsEdge,algorithm):
+    def findFirstTriangleNumberWithGraterNumberOfDivisors(self,noOfDivisorsEdge,noOfDivisorsAlgorithm):
         n = 1
-        noOfDivisors = algorithm(n)
+        noOfDivisors = noOfDivisorsAlgorithm(n)
         while noOfDivisors < noOfDivisorsEdge:
             n = n + 1
-            noOfDivisors = algorithm(n)
+            if noOfDivisorsAlgorithm.__name__ == 'calcNumberOfDivisorsForTriangleNumber':
+                noOfDivisors = noOfDivisorsAlgorithm(n)            
+            else:
+                noOfDivisors = noOfDivisorsAlgorithm(self.calcTriangle(n))                
+        return [self.calcTriangle(n),n,noOfDivisors]
+
+    def findFirstTriangleNumberWithGraterNumberOfDivisorsImprovedPrimes(self,noOfDivisorsEdge):
+        n = 1
+        noOfDivisors = self.calcNumberOfDivisorsUsingPrimes(n) * self.calcNumberOfDivisorsUsingPrimes((n+1)/2)
+        while noOfDivisors < noOfDivisorsEdge:
+            n = n + 1            
+            if n%2 == 0:
+                nPlus1 = self.calcNumberOfDivisorsUsingPrimes(n+1)
+                noOfDivisors = self.calcNumberOfDivisorsUsingPrimes(n/2) * nPlus1
+            else:
+                noOfDivisors = nPlus1 * self.calcNumberOfDivisorsUsingPrimes((n+1)/2)
         return [self.calcTriangle(n),n,noOfDivisors]
 
 if __name__ == "__main__":
-    triangle = TriangleNumber()
+    triangle = TriangleNumber(65000)
+    triangleImproved = TriangleNumber(1000)
 #    print triangle.calcNumberOfDivisors(53)
     print triangle.findFirstTriangleNumberWithGraterNumberOfDivisors(50,triangle.calcNumberOfDivisorsUsingPrimes)
     print triangle.findFirstTriangleNumberWithGraterNumberOfDivisors(50,triangle.calcNumberOfDivisorsUsingBruteForce)
-    print triangle.findFirstTriangleNumberWithGraterNumberOfDivisors(50,triangle.calcNumberOfDivisors)
+    print triangle.findFirstTriangleNumberWithGraterNumberOfDivisors(50,triangle.calcNumberOfDivisorsForTriangleNumber)
+    print triangleImproved.findFirstTriangleNumberWithGraterNumberOfDivisorsImprovedPrimes(50)
 #    print triangle.calcNumberOfDivisorsUsingPrimes(53)
--- src/test/problem12Test.py
+++ src/test/problem12Test.py
@@ -7,7 +7,7 @@ import problem00012
 
 class  Problem12TestCase(unittest.TestCase):
     def setUp(self):
-        self.triangle = problem00012.TriangleNumber()
+        self.triangle = problem00012.TriangleNumber(65000)
     
 
     #def tearDown(self):
@@ -16,8 +16,8 @@ class  Problem12TestCase(unittest.TestCase):
     
     def testImprovedNumberOfDivisorsAgainstBruteForce(self):
         for n in range(101,600):
-            self.assertEqual(self.triangle.calcNumberOfDivisorsUsingBruteForce(n),
-                             self.triangle.calcNumberOfDivisors(n))
+            self.assertEqual(self.triangle.calcNumberOfDivisorsUsingBruteForce(self.triangle.calcTriangle(n)),
+                             self.triangle.calcNumberOfDivisorsForTriangleNumber(n))
 
     def testfindFirstTriangleNumberWithGraterNumberOfDivisorsUsingBruteForce(self):
         result = self.triangle.findFirstTriangleNumberWithGraterNumberOfDivisors(50,
@@ -25,17 +25,38 @@ class  Problem12TestCase(unittest.TestCase):
         self.assertEqual([25200, 224, 90],result)
     def testfindFirstTriangleNumberWithGraterNumberOfDivisorsUsingImprovedAlgorithm(self):
         result = self.triangle.findFirstTriangleNumberWithGraterNumberOfDivisors(50,
-        self.triangle.calcNumberOfDivisors)
+        self.triangle.calcNumberOfDivisorsForTriangleNumber)
         self.assertEqual([25200, 224, 90],result)
     def testfindFirstTriangleNumberWithGraterNumberOfDivisorsUsingPrimeNumbers(self):
         result = self.triangle.findFirstTriangleNumberWithGraterNumberOfDivisors(50,
         self.triangle.calcNumberOfDivisorsUsingPrimes)
         self.assertEqual([25200, 224, 90],result)
+    def testfindFirstTriangleNumberWithGraterNumberOfDivisorsUsingPrimeNumbersImproved(self):
+        triangleImproved = problem00012.TriangleNumber(1000)
+        result = triangleImproved.findFirstTriangleNumberWithGraterNumberOfDivisorsImprovedPrimes(50)
+        self.assertEqual([25200, 224, 90],result)
     def testfindFirstTriangleNumberWithGraterNumberOfDivisorsUsingImprovedAlgorithmSolution(self):
         result = self.triangle.findFirstTriangleNumberWithGraterNumberOfDivisors(500,
-        self.triangle.calcNumberOfDivisors)
+        self.triangle.calcNumberOfDivisorsForTriangleNumber)
+        self.assertEqual([76576500, 12375, 576],result)
+    def testfindFirstTriangleNumberWithGraterNumberOfDivisorsUsingPrimes(self):
+        result = self.triangle.findFirstTriangleNumberWithGraterNumberOfDivisors(500,
+        self.triangle.calcNumberOfDivisorsUsingPrimes)
+        self.assertEqual([76576500, 12375, 576],result)
+    def testfindFirstTriangleNumberWithGraterNumberOfDivisorsUsingPrimesImproved(self):
+        triangleImproved = problem00012.TriangleNumber(1000)
+        result = triangleImproved.findFirstTriangleNumberWithGraterNumberOfDivisorsImprovedPrimes(500)
         self.assertEqual([76576500, 12375, 576],result)
 
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestSuite()
+#    suite.addTest(Problem12TestCase('testImprovedNumberOfDivisorsAgainstBruteForce'))
+#    suite.addTest(Problem12TestCase('testfindFirstTriangleNumberWithGraterNumberOfDivisorsUsingBruteForce'))
+#    suite.addTest(Problem12TestCase('testfindFirstTriangleNumberWithGraterNumberOfDivisorsUsingImprovedAlgorithm'))
+#    suite.addTest(Problem12TestCase('testfindFirstTriangleNumberWithGraterNumberOfDivisorsUsingPrimeNumbers'))
+#    suite.addTest(Problem12TestCase('testfindFirstTriangleNumberWithGraterNumberOfDivisorsUsingImprovedAlgorithmSolution'))
+#    suite.addTest(Problem12TestCase('testfindFirstTriangleNumberWithGraterNumberOfDivisorsUsingPrimeNumbersImproved'))
+    suite.addTest(Problem12TestCase('testfindFirstTriangleNumberWithGraterNumberOfDivisorsUsingPrimes'))
+#    suite.addTest(Problem12TestCase('testfindFirstTriangleNumberWithGraterNumberOfDivisorsUsingPrimesImproved'))
+    unittest.TextTestRunner(verbosity=2).run(suite)
 
